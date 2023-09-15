2e19fa7e6de53beb7cac6802a918dd83f8e54e23
23kartik <kartik23tiwari@gmai.com>
2023-09-15 14:14:45 +0530

    created jwt token

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,9 @@ const asyncHandler=require("express-async-handler");
 const User=require("../models/userModel");
 const bcrypt=require("bcrypt");
 
+const jwt=require("jsonwebtoken");
+
+
 //@dec Register a Users 
 //@route Post /api/Users/regiser
 //@access public
@@ -41,11 +44,34 @@ const postUser = asyncHandler(async (req,res)=>{
 //@access public
 
 const loginUser = asyncHandler(async (req,res)=>{
-    const {username,email,password}=req.body;
-    if(!username||!email||!password){
+    const {email,password}=req.body;
+    if(!email||!password){
         res.status(400);
         throw new Error("All fields are mandatory");
     }
+
+const user=await User.findOne({email});
+
+//compare password with hashed password
+if(user&& (await bcrypt.compare(password,user.password))){
+    const acessToken=jwt.sign({
+        user:{
+            username:user.username,
+            email:user.email,
+            id:user.id,
+        },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn:"1m"}
+    );
+    res.status(200).json({acessToken});
+}
+    else{
+        res.status(401);
+        throw new Error("email or password invalid");
+
+    }
+
     res.json({mesaage:"login user"})
 
 });
